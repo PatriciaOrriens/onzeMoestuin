f2ff569a9c58759b622ba498de86cdebf59c5e48
Patricia Orriens <patriciaspuij@hotmail.com>
2019-12-09 15:25:30 +0100

    Initial commit

---
+++ src/main/java/groentjes/onzeMoestuin/OnzeMoestuinApplication.java
@@ -0,0 +1,13 @@
+package groentjes.onzeMoestuin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnzeMoestuinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnzeMoestuinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/groentjes/onzeMoestuin/OnzeMoestuinApplicationTests.java
@@ -0,0 +1,13 @@
+package groentjes.onzeMoestuin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnzeMoestuinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
