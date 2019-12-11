c9f8180c3f1b5ae84d424abb2e9060b52db9cb75
ggwybenga <ggwybenga@yahoo.com>
2019-12-11 13:35:14 +0100

    added entity User added JPA annotations

--- src/main/java/groentjes/onzeMoestuin/model/User.java
+++ src/main/java/groentjes/onzeMoestuin/model/User.java
@@ -1,4 +1,41 @@
 package groentjes.onzeMoestuin.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity (name = "User")
 public class User {
+
+    @Id
+    @Column(name = "id")
+    private int userId;
+
+    private String name;
+
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
