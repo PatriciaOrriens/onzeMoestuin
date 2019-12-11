2b7b6a2eedb44c27323b67332887972042827967
Patricia Orriens <patriciaspuij@hotmail.com>
2019-12-11 10:18:36 +0100

    modellen User, Garden, Plant en PlantInformation gevuld met attributen; inclusief getters en setters foreign keys nog niet gespecificeerd.

--- src/main/java/groentjes/onzeMoestuin/model/User.java
+++ src/main/java/groentjes/onzeMoestuin/model/User.java
@@ -1,4 +1,51 @@
 package groentjes.onzeMoestuin.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+
+    private String emailAddress;
+    private String name;
+    private String password;
+
+    // getters and setters
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
