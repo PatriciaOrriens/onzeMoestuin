6313fd48b145d5991d548edf98804d268b38a379
ggwybenga <ggwybenga@yahoo.com>
2019-12-11 14:05:02 +0100

    added setter tests

--- src/test/java/groentjes/onzeMoestuin/OnzeMoestuinApplicationTests.java
+++ src/test/java/groentjes/onzeMoestuin/OnzeMoestuinApplicationTests.java
@@ -1,13 +1,32 @@
 package groentjes.onzeMoestuin;
 
+import groentjes.onzeMoestuin.model.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class OnzeMoestuinApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testSetUserId() {
+		User user = new User();
+		user.setUserId(4);
+		assertTrue(user.getUserId() == 1);
 	}
+
+	@Test
+	public void testSetName() {
+		User user = new User();
+		user.setName("Robin");
+		assertTrue(user.getName().equals("Batman"));
+	}
+
+	@Test
+	public void testSetPassword() {
+		User user = new User();
+		user.setPassword("OJ@#$!0*");
+		assertTrue(user.getPassword().equals("ABC#1"));
+	}
+
 
 }
