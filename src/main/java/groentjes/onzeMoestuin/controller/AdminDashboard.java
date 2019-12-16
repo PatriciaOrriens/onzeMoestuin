70c87d532c58df3b07a4bae123f3e3293b7a630f
Eric van Dalen <eric.vandalen@online.nl>
2019-12-16 10:44:50 +0100

    Controller klasse voor het adminstrator dashboard toegevoegd.

--- src/main/java/groentjes/onzeMoestuin/controller/AdminDashboard.java
+++ src/main/java/groentjes/onzeMoestuin/controller/AdminDashboard.java
@@ -1,4 +1,41 @@
 package groentjes.onzeMoestuin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Eric van Dalen
+ * Controller klasse voor het Adminstrator dashboard om taken te selecteren.
+ */
+@Controller
 public class AdminDashboard {
+    private List<String> menuOptions = new ArrayList<>();
+
+    @PostMapping("/onzemoestuin/admindashboard")
+    public String doWelcomeMenu(String menuOption) {
+        if(menuOption != null) {
+            return "redirect:/" + menuOption;
+        }
+        return "redirect:/onzemoestuin/admindashboard";
+    }
+
+    @GetMapping("/onzemoestuin/admindashboard")
+    public String getMenu(Model model) {
+        menuOptions.clear();
+        menuOptions.add("manageUser");
+        model.addAttribute("menuOptions", menuOptions);
+        return "onzemoestuin/admindashboard";
+    }
+
+    @PostMapping("/onzemoestuin/admindashboard")
+    public String backToMenu() {
+        return "redirect:/onzemoestuin/admindashboard";
+    }
 }
+
