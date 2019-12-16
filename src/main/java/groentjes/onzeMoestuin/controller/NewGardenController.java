b0dec1f989903b9f271de9c895acd4bcbfe612ab
wimkruizinga <wimkruizinga@gmail.com>
2019-12-16 11:02:17 +0100

    Modified NewGardenController: pass in logged-in user and fixed jsp path

--- src/main/java/groentjes/onzeMoestuin/controller/NewGardenController.java
+++ src/main/java/groentjes/onzeMoestuin/controller/NewGardenController.java
@@ -5,6 +5,9 @@ import groentjes.onzeMoestuin.model.Garden;
 import groentjes.onzeMoestuin.model.User;
 import groentjes.onzeMoestuin.repository.GardenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,22 +18,26 @@ import org.springframework.web.bind.annotation.PostMapping;
  * @author Patricia Orriens-Spuij
  * Controller for view in which user can create a new garden, or update his/her garden
  */
+@Controller
 public class NewGardenController {
 
     @Autowired
-    GardenRepository gardenRepository;
+    private GardenRepository gardenRepository;
 
     @GetMapping("/garden/add")
     protected String showGardenForm(Model model) {
         model.addAttribute("garden", new Garden());
-        model.addAttribute("user", new User());
-        return "newGardenForm";
+
+        //pass logged in user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("user", authentication);
+        return "newGarden";
     }
 
     @PostMapping({"/garden/add"})
     protected String saveOrUpdateGarden(@ModelAttribute("garden") Garden garden, BindingResult result) {
         if (result.hasErrors()) {
-            return "newGardenForm";
+            return "newGarden";
         } else {
             gardenRepository.save(garden);
             return "redirect:/garden";
