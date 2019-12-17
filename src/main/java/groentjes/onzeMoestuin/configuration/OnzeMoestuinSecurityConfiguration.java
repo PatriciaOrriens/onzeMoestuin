ebb5c2308de71eafe3d96c091924d4e6646ec226
wimkruizinga <wimkruizinga@gmail.com>
2019-12-17 15:13:09 +0100

    Opened registerUser endpoint to all users (including anonymous)

--- src/main/java/groentjes/onzeMoestuin/configuration/OnzeMoestuinSecurityConfiguration.java
+++ src/main/java/groentjes/onzeMoestuin/configuration/OnzeMoestuinSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +19,16 @@ public class OnzeMoestuinSecurityConfiguration extends WebSecurityConfigurerAdap
     @Autowired
     GardenUserDetailsService gardenUserDetailsService;
 
-    // in-memory saving of user information
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/registerUser").permitAll()
+                .anyRequest().authenticated()
+                .and();
+    }
+
+        // in-memory saving of user information
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
