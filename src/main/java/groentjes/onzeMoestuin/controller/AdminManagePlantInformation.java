b4d87eac940a2d9233d997e044f45b3a2dceb89a
Eric van Dalen <eric.vandalen@online.nl>
2019-12-18 17:13:40 +0100

    Overzicht van plantinfo voor beheerder toegevoegd.

--- src/main/java/groentjes/onzeMoestuin/controller/AdminManagePlantInformation.java
+++ src/main/java/groentjes/onzeMoestuin/controller/AdminManagePlantInformation.java
@@ -1,4 +1,31 @@
 package groentjes.onzeMoestuin.controller;
 
+import groentjes.onzeMoestuin.repository.PlantInformationRepository;
+import groentjes.onzeMoestuin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * @author Eric van Dalen
+ * Controller class for managing the plant information.
+ */
+@Controller
 public class AdminManagePlantInformation {
+
+    @Autowired
+    private PlantInformationRepository plantInformationRepository;
+
+    @Autowired UserRepository userRepository;
+
+    @GetMapping("/adminManagePlantInformation")
+    @Secured("ROLE_ADMIN")
+    public String managePlantInfo(Model model) {
+        model.addAttribute("plantInformation", plantInformationRepository.findAll());
+        //model.addAttribute("allUsers", userRepository.findAll());
+        return "adminManagePlantInformation";
+    }
 }
--- src/main/webapp/WEB-INF/views/adminManagePlantInformation.jsp
+++ src/main/webapp/WEB-INF/views/adminManagePlantInformation.jsp
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>User Overview </h1>
+        <table>
+                    <tr><th>Name </th><th></th></tr>
+        <c:forEach items="${plantInformation}" var="plant">
+            <tr>
+                <td><c:out value="${plant.plantName}" /></td>
+                <td><td><a href="/user/delete/<c:out value="${plant.plantName}" />">Delete</a></td></td>
+            </tr>
+        </c:forEach>
+        </table>
+        <a href="adminchangeplantinfo">Add new plant information</a>
+                <h1></h1>
+                <h1></h1>
+        <a href="/adminDashboard">Back to task menu</a>
 </html>
