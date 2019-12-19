1a77e57815d72b8ab23e9a15a10dbe4bff2b0c46
wimkruizinga <wimkruizinga@gmail.com>
2019-12-19 14:24:35 +0100

    Functionaliteit toegevoegd: PlantInformation objecten ophalen uit de database en weergeven in de addPlant view

--- src/main/java/groentjes/onzeMoestuin/controller/PlantController.java
+++ src/main/java/groentjes/onzeMoestuin/controller/PlantController.java
@@ -4,6 +4,7 @@ import groentjes.onzeMoestuin.model.Garden;
 import groentjes.onzeMoestuin.model.Plant;
 import groentjes.onzeMoestuin.model.PlantInformation;
 import groentjes.onzeMoestuin.repository.GardenRepository;
+import groentjes.onzeMoestuin.repository.PlantInformationRepository;
 import groentjes.onzeMoestuin.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,15 +23,20 @@ public class PlantController {
     @Autowired
     private GardenRepository gardenRepository;
 
+    @Autowired
+    private PlantInformationRepository plantInformationRepository;
+
     @GetMapping("/garden/{gardenId}/addPlant")
     public String getAddPlantForm(Model model, @PathVariable("gardenId") final Integer gardenId) {
         Optional<Garden> garden  = gardenRepository.findById(gardenId);
+
 
         if (garden.isPresent()) {
             Plant plant = new Plant();
             plant.setGarden(garden.get());
             model.addAttribute("plant", plant);
             model.addAttribute("garden", garden.get());
+            model.addAttribute("allPlantInformation", plantInformationRepository.findAll());
             return "addPlant";
         } else {
             return "redirect:/";
--- src/main/webapp/WEB-INF/views/addPlant.jsp
+++ src/main/webapp/WEB-INF/views/addPlant.jsp
@@ -5,12 +5,25 @@
 
 	<h1>Plant toevoegen aan ${garden.gardenName}</h1>
 
+		<table class="table">
+    		<tr>
+    			<th>Naam</th>
+    		</tr>
+    		<c:forEach items="${allPlantInformation}" var="plantInfo">
+    			<tr>
+    				<td>
+    					<c:out value="${plantInfo.plantName}" />
+    				</td>
+    				<td>
+    				    <a href="/garden/<c:out value="${garden.gardenId}" />/<c:out value="${plantInfo.plantInfoId}" />">Toevoegen</a>
+    				</td>
+    			</tr>
+    		</c:forEach>
+    	</table>
+
 	<!-- JSTL form -->
     <form:form action="/addplant" modelAttribute="plant">
         <div class="form-group">
-
-
-
         </div>
         <form:button type="submit" class="btn btn-primary">Voeg toe</form:button>
 	</form:form>
