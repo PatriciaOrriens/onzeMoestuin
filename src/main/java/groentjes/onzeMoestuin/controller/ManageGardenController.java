e2c9f62a5def090f755e3cbab5ec14e4f2680f40
Eric van Dalen <eric.vandalen@online.nl>
2019-12-20 12:56:10 +0100

    Overzichtscherm van tuinen toegevoegd met de mogelijkheid om een tuin te verwijderen

--- src/main/java/groentjes/onzeMoestuin/controller/ManageGardenController.java
+++ src/main/java/groentjes/onzeMoestuin/controller/ManageGardenController.java
@@ -1,6 +1,40 @@
 package groentjes.onzeMoestuin.controller;
 
+import groentjes.onzeMoestuin.model.Garden;
+import groentjes.onzeMoestuin.model.PlantInformation;
+import groentjes.onzeMoestuin.repository.GardenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Optional;
+
+/**
+ * @author Eric van Dalen
+ * Controller class to welcome user and to manage the gardens
+ */
+@Controller
 public class ManageGardenController {
 
-    // give a welcome message here
+    @Autowired
+    GardenRepository gardenRepository;
+
+    @GetMapping("/userManageGardens")
+    public String findAllYourGardens(Model model) {
+// TODO: laat alleen tuinen zien van deze logged-in user
+        model.addAttribute("allYourGardens", gardenRepository.findAll());
+        return "/manageGarden";
+    }
+
+    @GetMapping("/user/garden/delete/{gardenId}")
+    public String deleteGarden(@ModelAttribute("gardenId") Integer gardenId, BindingResult result) {
+        Optional<Garden> garden = gardenRepository.findById(gardenId);
+        garden.ifPresent(information -> gardenRepository.delete(information));
+        return "redirect:/userManageGardens";
+    }
+
 }
--- src/main/webapp/WEB-INF/views/manageGarden.jsp
+++ src/main/webapp/WEB-INF/views/manageGarden.jsp
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>Tuin Overzicht </h1>
+        <table>
+            <tr><th>Tuinnaam </th><th></th><th></th></tr>
+            <c:forEach items="${allYourGardens}" var="garden">
+                <tr>
+                    <td><c:out value="${garden.gardenName}" /></td>
+                    <td></td>
+                    <td><a href="/garden/<c:out value="${garden.gardenId}" />">Toon</a></td>
+                    <td><a href="/user/garden/delete/<c:out value="${garden.gardenId}" />">Verwijder</a></td>
+                </tr>
+            </c:forEach>
+        </table>
+        <h1></h1>
+        <a href="/garden/add">Voeg tuin toe</a>
+        <h1></h1>
+        <h1></h1>
+        <a href="/logout">Logout</a>
+    </body>
 </html>
