683e55e03b2f2d3d897ca1830bb0cb311361b548
Eric van Dalen <eric.vandalen@online.nl>
2020-01-09 21:33:14 +0100

    security improvement to prevent that a person who is not a member/user of a garden can add a plant in that garden.

--- src/main/java/groentjes/onzeMoestuin/controller/PlantController.java
+++ src/main/java/groentjes/onzeMoestuin/controller/PlantController.java
@@ -3,10 +3,12 @@ package groentjes.onzeMoestuin.controller;
 import groentjes.onzeMoestuin.model.Garden;
 import groentjes.onzeMoestuin.model.Plant;
 import groentjes.onzeMoestuin.model.PlantInformation;
+import groentjes.onzeMoestuin.model.User;
 import groentjes.onzeMoestuin.repository.GardenRepository;
 import groentjes.onzeMoestuin.repository.PlantInformationRepository;
 import groentjes.onzeMoestuin.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+/**
+ * @author Eric van Dalen
+ * Controller for managing the plants in a garden
+ */
 @Controller
 public class PlantController {
 
@@ -27,19 +33,21 @@ public class PlantController {
     private PlantInformationRepository plantInformationRepository;
 
     @GetMapping("/garden/{gardenId}/addPlant")
-    public String getAddPlantForm(Model model, @PathVariable("gardenId") final Integer gardenId) {
-        Optional<Garden> garden = gardenRepository.findById(gardenId);
+    public String getAddPlantForm(Model model, @PathVariable("gardenId") final Integer gardenId,
+                                  @AuthenticationPrincipal User user) {
 
+        Optional<Garden> garden = gardenRepository.findById(gardenId);
         if (garden.isPresent()) {
-            Plant plant = new Plant();
-            plant.setGarden(garden.get());
-            model.addAttribute("plant", plant);
-            model.addAttribute("garden", garden.get());
-            model.addAttribute("allPlantInformation", plantInformationRepository.findAll());
-            return "addPlant";
-        } else {
-            return "redirect:/";
+            if(garden.get().isGardenMember(user)) {
+                Plant plant = new Plant();
+                plant.setGarden(garden.get());
+                model.addAttribute("plant", plant);
+                model.addAttribute("garden", garden.get());
+                model.addAttribute("allPlantInformation", plantInformationRepository.findAll());
+                return "addPlant";
+            }
         }
+            return "redirect:/";
     }
 
     @GetMapping("/plant/{plantId}")
@@ -57,18 +65,20 @@ public class PlantController {
 
     @PostMapping("/garden/{gardenId}/addPlant")
     public String addPlantToGarden(@RequestParam("plantInfoId") Integer plantInfoId, @ModelAttribute("plant") Plant plant,
-                                   BindingResult result, @PathVariable("gardenId") final Integer gardenId) {
+                                   BindingResult result, @PathVariable("gardenId") final Integer gardenId,
+                                   @AuthenticationPrincipal User user) {
 
         Optional<PlantInformation> plantInfo  = plantInformationRepository.findById(plantInfoId);
         Optional<Garden> garden = gardenRepository.findById(gardenId);
 
         if (plantInfo.isPresent() && garden.isPresent()) {
-            plant.setPlantInformation(plantInfo.get());
-            plant.setGarden(garden.get());
-            plantRepository.save(plant);
-            return "redirect:/garden/" + gardenId;
-        } else {
-            return "redirect:/";
+            if(garden.get().isGardenMember(user)) {
+                plant.setPlantInformation(plantInfo.get());
+                plant.setGarden(garden.get());
+                plantRepository.save(plant);
+                return "redirect:/garden/" + gardenId;
+            }
         }
+        return "redirect:/";
     }
 }
