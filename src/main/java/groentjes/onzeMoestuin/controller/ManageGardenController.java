fe000bdbcb05cbda070f38634f57a78705f0d92b
wimkruizinga <wimkruizinga@gmail.com>
2020-01-10 13:31:54 +0100

    Find user by username via RequestParam

--- src/main/java/groentjes/onzeMoestuin/controller/ManageGardenController.java
+++ src/main/java/groentjes/onzeMoestuin/controller/ManageGardenController.java
@@ -4,14 +4,17 @@ import groentjes.onzeMoestuin.model.Garden;
 import groentjes.onzeMoestuin.model.Plant;
 import groentjes.onzeMoestuin.model.User;
 import groentjes.onzeMoestuin.repository.GardenRepository;
+import groentjes.onzeMoestuin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -25,6 +28,9 @@ public class ManageGardenController {
 
     @Autowired
     private GardenRepository gardenRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/userManageGardens")
     public String findAllYourGardens(Model model, @AuthenticationPrincipal User gardenuser) {
@@ -40,11 +46,16 @@ public class ManageGardenController {
     }
 
     @GetMapping("/garden/{gardenId}/invite")
-    protected String inviteToGarden(Model model, @PathVariable("gardenId") final Integer gardenId) {
+    protected String inviteToGarden(Model model, @PathVariable("gardenId") final Integer gardenId,
+                                    @RequestParam(value = "search") Optional<String> search) {
         Optional<Garden> garden = gardenRepository.findById(gardenId);
 
         if (garden.isPresent()) {
             model.addAttribute("garden", garden.get());
+            if (search.isPresent()) {
+                Optional<User> foundUser = userRepository.findByUsername(search.get());
+                foundUser.ifPresent(user -> model.addAttribute("foundUser", user));
+            }
             return "inviteGardenMember";
         }
         return "redirect:/";
--- src/main/webapp/WEB-INF/views/inviteGardenMember.jsp
+++ src/main/webapp/WEB-INF/views/inviteGardenMember.jsp
@@ -5,13 +5,20 @@
     <h1 class="display-3">Iemand uitnodigen voor '${garden.gardenName}'</h1>
 
     <p>Voer de naam van de gebruiker in om deze uit te nodigen.</p>
-    <!-- JSTL form -->
-        <form:form action="/garden/${garden.gardenId}/invite">
+    <!-- JSTL form
+        <form:form action="/garden/${garden.gardenId}/invite" method="get" modelAttribute = "search">
             <div class="form-group">
                 <label for="name">Naam:</label>
-                <form:input class="form-control" />
+                <form:input path="search" class="form-control" />
             </div>
-            <form:button type="submit" class="btn btn-primary">Uitnodigen</form:button>
-    	</form:form>
+            <form:button type="submit" class="btn btn-primary">Zoek</form:button>
+    	</form:form> -->
+
+
+    <c:if test="${!empty foundUser}">
+        <h2>Gevonden gebruiker:</h2>
+        <p>${foundUser.username}</p>
+    </c:if>
+
 
 <c:import url="partials/footer.jsp" />
