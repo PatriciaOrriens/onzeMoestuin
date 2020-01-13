be813c581beba7ee9183f3ee4f902be4e1f7a5fe
ggwybenga <ggwybenga@yahoo.com>
2020-01-13 10:34:09 +0100

    fixed not being able to logout

--- src/main/java/groentjes/onzeMoestuin/configuration/OnzeMoestuinSecurityConfiguration.java
+++ src/main/java/groentjes/onzeMoestuin/configuration/OnzeMoestuinSecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -32,9 +33,9 @@ public class OnzeMoestuinSecurityConfiguration extends WebSecurityConfigurerAdap
                     .failureUrl("/loginfailed")
                     .permitAll()
                 .and()
-                    .logout()
-                    .logoutUrl("/logout")
-                    .permitAll();
+                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/logout.done").deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true);
     }
 
     // in-memory saving of user information
