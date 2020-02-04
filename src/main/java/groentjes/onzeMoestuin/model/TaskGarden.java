4878df980ecaf6bc978b6d82a6f3e2c6b5d95da8
Eric van Dalen <eric.vandalen@online.nl>
2020-02-04 10:15:44 +0100

    TaskGarden adapted

--- src/main/java/groentjes/onzeMoestuin/model/TaskGarden.java
+++ src/main/java/groentjes/onzeMoestuin/model/TaskGarden.java
@@ -1,6 +1,12 @@
 package groentjes.onzeMoestuin.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  * @author Eric van Dalen
@@ -10,6 +16,11 @@ import javax.persistence.Entity;
 public class TaskGarden extends Task {
 
     private String taskGardenName;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "garden_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Garden garden;
 
     public String getTaskGardenName() {
         return taskGardenName;
