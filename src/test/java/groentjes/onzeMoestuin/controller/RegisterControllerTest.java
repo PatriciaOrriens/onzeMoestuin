79d48a400cf9edcef11efab3a32d1bd623c61c17
ggwybenga <ggwybenga@yahoo.com>
2020-02-13 16:21:46 +0100

    fixing more controller tests

--- src/test/java/groentjes/onzeMoestuin/controller/RegisterControllerTest.java
+++ src/test/java/groentjes/onzeMoestuin/controller/RegisterControllerTest.java
@@ -1,106 +1,107 @@
-//package groentjes.onzeMoestuin.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import groentjes.onzeMoestuin.model.User;
-//import groentjes.onzeMoestuin.repository.GardenInvitationRepository;
-////import groentjes.onzeMoestuin.repository.RoleRepository;
-//import groentjes.onzeMoestuin.repository.UserRepository;
+package groentjes.onzeMoestuin.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import groentjes.onzeMoestuin.model.User;
+import groentjes.onzeMoestuin.repository.GardenInvitationRepository;
+//import groentjes.onzeMoestuin.repository.RoleRepository;
+import groentjes.onzeMoestuin.repository.RoleRepository;
+import groentjes.onzeMoestuin.repository.UserRepository;
 //import groentjes.onzeMoestuin.service.GardenUserDetailsService;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Mockito;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.ArgumentCaptor.forClass;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-///**
-// * @author Wim Kruizinga and Eric van Dalen
-// * Tests concerning registration of users
-// */
-//@ExtendWith(SpringExtension.class)
-//@WebMvcTest(controllers = RegisterController.class)
-//class RegisterControllerTest {
-//
-//    private static final String USERNAME = "gebruikersnaam";
-//    private static final String PASSWORD = "wachtwoord";
-//    private static final String EMAIL = "gebruiker@email.com";
-//    private User otherUser = new User();
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @MockBean
-//    private UserRepository userRepository;
-//
-//    /*@MockBean
-//    private RoleRepository roleRepository;
-//*/
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author Wim Kruizinga and Eric van Dalen
+ * Tests concerning registration of users
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = RegisterController.class)
+class RegisterControllerTest {
+
+    private static final String USERNAME = "gebruikersnaam";
+    private static final String PASSWORD = "wachtwoord";
+    private static final String EMAIL = "gebruiker@email.com";
+    private User otherUser = new User();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private RoleRepository roleRepository;
+
 //    @MockBean
 //    private GardenUserDetailsService gardenUserDetailsService;
-//
-//    @MockBean
-//    private GardenInvitationRepository gardenInvitationRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        otherUser.setUsername(USERNAME);
-//        otherUser.setEmail(EMAIL);
-//        otherUser.setPassword(PASSWORD);
-//    }
-//
-//    @Test
-//    void testGetRegisterUserForm() throws Exception {
-//        final ResultActions result = mockMvc.perform(get("/registerUser"))
-//                .andExpect(status().isOk())
-//                .andExpect(forwardedUrl("/WEB-INF/views/register.jsp"));
-//    }
-//
-//    @Test
-//    void testSaveNewUserWithAnExistingUsername() throws Exception {
-//        Mockito.when((userRepository.findByUsername(USERNAME))).thenReturn(Optional.of(otherUser));
-//
-//        mockMvc.perform(post("/registerUser").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("username" , USERNAME).param("email", EMAIL).param("password", PASSWORD)
-//                .flashAttr("user", new User()).with(csrf())).andExpect(status().isOk())
-//                .andExpect(view().name("register"))
-//                .andExpect(forwardedUrl("/WEB-INF/views/register.jsp"));
-//    }
-//
-//    @Test
-//    void testSaveNewUser() throws Exception {
-//
-//        Mockito.when((userRepository.findByUsername(USERNAME))).thenReturn(Optional.empty());
-//
-//        mockMvc.perform(post("/registerUser").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("username" , USERNAME).param("email", EMAIL).param("password", PASSWORD)
-//                .flashAttr("user", new User()).with(csrf())).andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/login"))
-//                .andExpect(redirectedUrl("/login"));
-//
-//        ArgumentCaptor<User> formObjectArgument = forClass(User.class);
-//        verify(userRepository, times(1)).save(formObjectArgument.capture());
-//
-//        User formObject = formObjectArgument.getValue();
-//        Assertions.assertThat(formObject.getUsername()).isEqualTo(USERNAME);
-//        Assertions.assertThat(formObject.getEmail()).isEqualTo(EMAIL);
-//    }
-//}
+
+    @MockBean
+    private GardenInvitationRepository gardenInvitationRepository;
+
+    @BeforeEach
+    void setUp() {
+        otherUser.setUsername(USERNAME);
+        otherUser.setEmail(EMAIL);
+        otherUser.setPassword(PASSWORD);
+    }
+
+    @Test
+    void testGetRegisterUserForm() throws Exception {
+        final ResultActions result = mockMvc.perform(get("/registerUser"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/views/register.jsp"));
+    }
+
+    @Test
+    void testSaveNewUserWithAnExistingUsername() throws Exception {
+        Mockito.when((userRepository.findByUsername(USERNAME))).thenReturn(Optional.of(otherUser));
+
+        mockMvc.perform(post("/registerUser").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username" , USERNAME).param("email", EMAIL).param("password", PASSWORD)
+                .flashAttr("user", new User()).with(csrf())).andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(forwardedUrl("/WEB-INF/views/register.jsp"));
+    }
+
+    @Test
+    void testSaveNewUser() throws Exception {
+
+        Mockito.when((userRepository.findByUsername(USERNAME))).thenReturn(Optional.empty());
+
+        mockMvc.perform(post("/registerUser").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username" , USERNAME).param("email", EMAIL).param("password", PASSWORD)
+                .flashAttr("user", new User()).with(csrf())).andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/login"))
+                .andExpect(redirectedUrl("/login"));
+
+        ArgumentCaptor<User> formObjectArgument = forClass(User.class);
+        verify(userRepository, times(1)).save(formObjectArgument.capture());
+
+        User formObject = formObjectArgument.getValue();
+        Assertions.assertThat(formObject.getUsername()).isEqualTo(USERNAME);
+        Assertions.assertThat(formObject.getEmail()).isEqualTo(EMAIL);
+    }
+}
--- src/test/java/groentjes/onzeMoestuin/controller/TaskPlantControllerTest.java
+++ src/test/java/groentjes/onzeMoestuin/controller/TaskPlantControllerTest.java
@@ -1,120 +1,123 @@
-//package groentjes.onzeMoestuin.controller;
-//
-//import groentjes.onzeMoestuin.model.*;
-//import groentjes.onzeMoestuin.repository.*;
+package groentjes.onzeMoestuin.controller;
+
+import groentjes.onzeMoestuin.model.*;
+import groentjes.onzeMoestuin.repository.*;
 //import groentjes.onzeMoestuin.service.GardenUserDetailsService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mockito;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//import java.util.Optional;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-///**
-// * @author Eric van Dalen
-// * Tests concerning the functionality around task plant completion
-// */
-//@ExtendWith(SpringExtension.class)
-//@WebMvcTest(controllers = TaskPlantController.class)
-//class TaskPlantControllerTest {
-//
-//    private static final int ONE = 1;
-//    private static final int TWO = 2;
-//    private static final int THREE = 3;
-//    private static final int EIGHT = 8;
-//    private static final String DATE = "25-01-2020";
-//
-//    private TaskPlant taskPlant;
-//    private TaskPlantInfo taskPlantInfo;
-//    private Plant plant;
-//    private Garden garden;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import java.util.Optional;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Eric van Dalen
+ * Tests concerning the functionality around task plant completion
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = TaskPlantController.class)
+class TaskPlantControllerTest {
+
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int EIGHT = 8;
+    private static final String DATE = "25-01-2020";
+
+    private TaskPlant taskPlant;
+    private TaskPlantInfo taskPlantInfo;
+    private Plant plant;
+    private Garden garden;
+
+    @Autowired
+    private MockMvc mockMvc;
+
 //    @MockBean
 //    private GardenUserDetailsService gardenUserDetailsService;
-//
-////
-//
-//
-//    @MockBean
-//    private PlantRepository plantRepository;
-//
-//    @MockBean
-//    private GardenRepository gardenRepository;
-//
-//    @MockBean
-//    private TaskPlantRepository taskPlantRepository;
-//
-//    @MockBean
-//    private TaskGardenRepository taskGardenRepository;
-//
-//    @MockBean
-//    private TaskRepository taskRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        taskPlant = new TaskPlant();
-//        taskPlant.setTaskId(TWO);
-//        taskPlantInfo = new TaskPlantInfo();
-//        taskPlantInfo.setDaysAfterStart(EIGHT);
-//        taskPlant.setTaskPlantInfo(taskPlantInfo);
-//        taskPlant.setDueDate(DATE);
-//        taskPlant.setCompletedDate(null);
-//        taskPlant.setUser(null);
-//        plant = new Plant();
-//        taskPlant.setPlant(plant);
-//        garden = new Garden();
-//        garden.setGardenId(ONE);
-//        plant.setGarden(garden);
-//    }
-//
-//    @Test
-//    @WithMockUser(roles = "USER")
-//    void testProcessCompletedTaskPlantWithExistingTaskPlant() throws Exception {
-//        String taskId = taskPlant.getTaskId().toString();
-//
-//        boolean expectedisCompleted = true;
-//
-//        Mockito.when(taskRepository.findById(TWO))
-//                .thenReturn(Optional.ofNullable(taskPlant));
-//
-//        final ResultActions result = mockMvc.perform(get("/user/task/completed/" + taskId)
-//                .sessionAttr("taskId", taskId)).andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/garden/" + garden.getGardenId()));
-//
-//        boolean foundisCompleted = (taskPlant.getCompletedDate() != null);
-//
-//        Assertions.assertEquals(expectedisCompleted, foundisCompleted);
-//    }
-//
-//    @Test
-//    @WithMockUser(roles = "USER")
-//    void testProcessCompletedTaskPlantWithNonExistingTaskPlant() throws Exception {
-//        String taskId = taskPlant.getTaskId().toString();
-//
-//        boolean expectedisCompleted = false;
-//
-//        Mockito.when(taskRepository.findById(THREE))
-//                .thenReturn(Optional.ofNullable(taskPlant));
-//
-//        final ResultActions result = mockMvc.perform(get("/user/task/completed/" + taskId)
-//                .sessionAttr("taskId", taskId)).andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/" ));
-//
-//        boolean foundisCompleted = (taskPlant.getCompletedDate() != null);
-//
-//        Assertions.assertEquals(expectedisCompleted, foundisCompleted);
-//    }
-//}
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private PlantRepository plantRepository;
+
+    @MockBean
+    private GardenRepository gardenRepository;
+
+    @MockBean
+    private TaskPlantRepository taskPlantRepository;
+
+    @MockBean
+    private TaskGardenRepository taskGardenRepository;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    void setUp() {
+        taskPlant = new TaskPlant();
+        taskPlant.setTaskId(TWO);
+        taskPlantInfo = new TaskPlantInfo();
+        taskPlantInfo.setDaysAfterStart(EIGHT);
+        taskPlant.setTaskPlantInfo(taskPlantInfo);
+        taskPlant.setDueDate(DATE);
+        taskPlant.setCompletedDate(null);
+        taskPlant.setUser(null);
+        plant = new Plant();
+        taskPlant.setPlant(plant);
+        garden = new Garden();
+        garden.setGardenId(ONE);
+        plant.setGarden(garden);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void testProcessCompletedTaskPlantWithExistingTaskPlant() throws Exception {
+        String taskId = taskPlant.getTaskId().toString();
+
+        boolean expectedisCompleted = true;
+
+        Mockito.when(taskRepository.findById(TWO))
+                .thenReturn(Optional.ofNullable(taskPlant));
+
+        final ResultActions result = mockMvc.perform(get("/user/task/completed/" + taskId)
+                .sessionAttr("taskId", taskId)).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/garden/" + garden.getGardenId()));
+
+        boolean foundisCompleted = (taskPlant.getCompletedDate() != null);
+
+        Assertions.assertEquals(expectedisCompleted, foundisCompleted);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void testProcessCompletedTaskPlantWithNonExistingTaskPlant() throws Exception {
+        String taskId = taskPlant.getTaskId().toString();
+
+        boolean expectedisCompleted = false;
+
+        Mockito.when(taskRepository.findById(THREE))
+                .thenReturn(Optional.ofNullable(taskPlant));
+
+        final ResultActions result = mockMvc.perform(get("/user/task/completed/" + taskId)
+                .sessionAttr("taskId", taskId)).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/" ));
+
+        boolean foundisCompleted = (taskPlant.getCompletedDate() != null);
+
+        Assertions.assertEquals(expectedisCompleted, foundisCompleted);
+    }
+}
